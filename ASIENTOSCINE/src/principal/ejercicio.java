7d0582d8507ebe3d7e4a4f664a7fab6a510a5466
SamuelBeltreDAM1 <samubelme34ec@linkiafp.online>
2024-01-09 13:13:57 +0100

    ocupado o libre

--- ASIENTOSCINE/src/principal/ejercicio.java
+++ ASIENTOSCINE/src/principal/ejercicio.java
@@ -20,7 +20,22 @@ for (int i = 0; i<asientos.length; i++) {
 	}
 	System.out.println();
 }
+Scanner sc = new Scanner (System.in);
+System.out.print("INTRODUCE EL NUEMRO DE FILA: ");
+int fila=sc.nextInt();
+System.out.print("INTORDUCE EL NUMERO DE COLUMNA: ");
+int columna=sc.nextInt();
+for (int i = 0; i<1; i++) {
+	for (int j =0; j<1; j++) {
+		if (fila%2==0 && columna%2==0) {
+			 System.out.print("Esta ocupado, tienes que elegir uno que este libre");
+		}else {
+			System.out.println("perfecto, el asiento que has seleccionado esta libre");
 
+		}
+		System.out.println();
+	  }
 	}
 
+	}
 }
